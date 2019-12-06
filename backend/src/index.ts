1454a71915e3c58acbe1be77d6a189986a4468a1
Jam Risser <jam@codejam.ninja>
2019-12-06 08:40:13 +0000

    Use express for apollo server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
 import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server';
+import express, { Request, Response } from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import { createContext } from './context';
 import { createSchema } from './schema';
+
+const app = express();
+
+export const config = {
+  port: 4000
+};
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.redirect('/graphql');
+});
 
 (async () => {
   const server = new ApolloServer({
@@ -9,6 +20,12 @@ import { createSchema } from './schema';
     playground: true,
     context: createContext
   });
-  const { url } = await server.listen({ port: 4000 });
-  console.log(`server ready at ${url}`);
+  server.applyMiddleware({ app });
+  await new Promise((resolve, reject) => {
+    app.listen({ port: config.port }, (err?: Error) => {
+      if (err) return reject(err);
+      return resolve();
+    });
+  });
+  console.log(`listening on port ${config.port}`);
 })().catch(console.error);
